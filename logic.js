b71fd1c87ef7cc6989dfa7467f0f2d80edfe3c85
milo <mroberts18@bristolcc.edu>
2023-10-13 21:29:39 -0400

    Added logic to close nav bar and stop it from obscuring text upon using internal links

--- logic.js
+++ logic.js
@@ -4,7 +4,16 @@ const mobileNavButton = document.querySelector('#mobile-nav-button');
 const iconMenu = document.querySelector('#icon-menu');
 const iconCancel = document.querySelector('#icon-cancel');
 const primaryNavWrapper = document.querySelector('#primary-nav-wrapper');
+const navigationBarLinks = [...document.querySelectorAll('.navigation-bar-link')];
 
 mobileNavButton.addEventListener('click', () => {
     toggleDisplay(iconMenu, iconCancel, primaryNavWrapper);
+});
+
+navigationBarLinks.map((link) => {
+    link.addEventListener('click', (event) => {
+        if (!primaryNavWrapper.classList.contains('display-none')) {
+            toggleDisplay(iconMenu, iconCancel, primaryNavWrapper);
+        }
+    });
 });
